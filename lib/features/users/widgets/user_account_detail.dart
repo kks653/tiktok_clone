0c5298c088a1e1ec07a62b93b803855a26ddea10
Kyung Kook Son <kyungkook.son@gmail.com>
2023-08-28 22:58:04 +0900

    feat: add user profile screen details

---
+++ lib/features/users/widgets/user_account_detail.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants/gaps.dart';
+import '../../../constants/sizes.dart';
+
+class UserAccountDetail extends StatelessWidget {
+  const UserAccountDetail({
+    super.key,
+    required this.title,
+    required this.number,
+  });
+
+  final String number;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          number,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: Sizes.size18,
+          ),
+        ),
+        Gaps.v3,
+        Text(
+          title,
+          style: TextStyle(
+            color: Colors.grey.shade500,
+          ),
+        ),
+      ],
+    );
+  }
+}
