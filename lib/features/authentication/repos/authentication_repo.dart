d348b1854ff2c8ebe6449773d49aa77f38dab7e9
Kyung Kook Son <kyungkook.son@gmail.com>
2023-09-01 10:03:13 +0900

    feat: working on sign in logics

--- lib/features/authentication/repos/authentication_repo.dart
+++ lib/features/authentication/repos/authentication_repo.dart
@@ -7,8 +7,21 @@ class AuthenticationRepository {
   User? get _user => _firebaseAuth.currentUser;
   bool get isLoggedIn => _user != null;
 
+  Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
+
   Future<void> signUp(String email, String password) async {
     await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  Future<void> signIn(String email, String password) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
@@ -16,3 +29,9 @@ class AuthenticationRepository {
 }
 
 final authRepo = Provider((ref) => AuthenticationRepository());
+final authStateStream = StreamProvider(
+  (ref) {
+    final repo = ref.read(authRepo);
+    return repo.authStateChanges();
+  },
+);
