42c57dbb444fdbb14b5beef0bef4bf96dd89ab4e
Kyung Kook Son <kyungkook.son@gmail.com>
2023-09-03 12:03:57 +0900

    fix: downgrade engine to use ffmpeg in firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,23 +23,24 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export const onVideoCreated = functions.firestore.document("videos/{videoId}").onCreate(async (snapshot, context) => {
-    //await snapshot.ref.update({hello : "from functions"});
-    const spawn = require("child-process-promise").spawn;
-    const video = snapshot.data();
-    await spawn("ffmpeg", [
-        "-i",
-        video.fileUrl,
-        "-ss",
-        "00:00:01.000",
-        "-vframes",
-        "1",
-        "-vf",
-        "scale=150:-1",
-        `/tmp/${snapshot.id}.jpg`
-    ]);
-    const storgae = admin.storage();
-    storgae.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
-        destination:`thumbnails/${snapshot.id}.jpg`,
-    })
+export const onVideoCreated = functions.firestore
+    .document("videos/{videoId}")
+    .onCreate(async (snapshot, context) => {
+        const spawn = require("child-process-promise").spawn;
+        const video = snapshot.data();
+        await spawn("ffmpeg", [
+            "-i",
+            video.fileUrl,
+            "-ss",
+            "00:00:01.000",
+            "-vframes",
+            "1",
+            "-vf",
+            "scale=150:-1",
+            `/tmp/${snapshot.id}.jpg`
+        ]);
+        const storgae = admin.storage();
+        await storgae.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+            destination:`thumbnails/${snapshot.id}.jpg`,
+        })
 });
